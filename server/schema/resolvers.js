2858a53a8e275498e7e3e2f853b883f1762e978e
Cinosargo22 <cinosargo22@gmail.com>
2023-01-08 16:16:15 -0800

    tenth

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,59 +1,71 @@
-const { User, Book } = require('../models');
-const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
+            // return await User.findOne({
+            //     $or: [{ _id: context.user ? context.user._id : args.id }, { username: args.username }],
+            // });
+           
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
-                .select('-__v -password')
-                return userData;
-            }
-            throw new AuthenticationError('Not logged in');
+                return User.findOne({ _id: context.user._id });
+              }
+            //   throw new AuthenticationError('You need to be logged in!');
         }
     },
+
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, args) => {
+            const user = await User.findOne({ $or: [{ username: args.username }, { email: args.email }] });
+
+            if (!user) {
+                throw new AuthenticationError("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(args.password);
+
+            if (!correctPw) {
+                throw new AuthenticationError("Wrong password!");
+            }
+
+            const token = signToken(user);
 
             return { token, user };
         },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne( { email });
-            if (!user) {
-                throw new AuthenticationError('Incorrect credentials')
-            }
-            const correctPw = await user.isCorrectPassword(password);
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials')
-            }
-            const token = signToken(user);
-            return { token, user };
-        },
-        saveBook: async (parent, { book }, context) => {
+        saveBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
-                    { $addToSet: {savedBooks: book} },
-                    { new: true }
-                )
+                    { $addToSet: { savedBooks: args.input } },
+                    { new: true, runValidators: true }
+                );
                 return updatedUser;
             }
-            throw new AuthenticationError('You need to be logged in!')
+            // If user attempts to execute this mutation and isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in!');
         },
-        removeBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, args, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
-                    {_id: context.user._id},
-                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
                     { new: true }
-                )
+                );
                 return updatedUser;
             }
+            // If user attempts to execute this mutation and isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
-  };
-  
-  module.exports = resolvers;
+};
+
+module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,33 +1,33 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = process.env.SECRET;
-const expiration = "2h";
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // We split the token string into an array and return actual token
     if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
+      token = token.split(' ').pop().trim();
     }
 
     if (!token) {
       return req;
     }
 
-    // verify token and get user data out of it
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log("Invalid token");
+      console.log('Invalid token');
     }
 
+    // return the request object so it can be passed to the resolver as `context`
     return req;
   },
   signToken: function ({ username, email, _id }) {
